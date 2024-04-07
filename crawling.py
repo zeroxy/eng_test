dbc2f6f8f82bd3acec7d9cbd28ee6d4aab38a014
zeroxy <zeroxy@users.noreply.github.com>
2024-04-07 22:04:39 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -32,7 +32,7 @@ def main():
     table_text = table_text+'\n'.join(table_content)
     print(table_text)
     current_datetime = datetime.now().strftime("%Y-%m-%d")
-    filename = f"data_{current_datetime}.txt" 
+    filename = f"data_{current_datetime}.md" 
     with open(filename, 'w') as file:
         file.write(table_text)
 
