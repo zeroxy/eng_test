294d7b0bedc896181ec245cf251eed8bc6dc7c70
zeroxy <zeroxy@users.noreply.github.com>
2024-04-11 22:22:17 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -48,6 +48,8 @@ def get_llm_response_md(news_headlines_json, doc_length = 5, llm = LLM):
 2. 개별 뉴스를 양식에 맞춰서 한글로 번역해.
 3. 각각의 중요 핵심단어를 3~5개정도 정리해주고, '영어단어(한글뜻)' 형태로 출력해.
 4. 무조건 {doc_length}개의 JSON object list 형태로 변환해서 출력해. 출력전에 json spec 에 맞도록 sanitize를 꼭 해야해.
+5. python 의 json.loads 함수로 deserialize 할 때 문제가 될 만한 character 는 적절하게 변경해.
+6. code block 으로 출력 하지 마.
 json_obj = '''
     prompt = ChatPromptTemplate.from_template(prompt_head + '{news_headlines}'+ prompt_tail)
 
